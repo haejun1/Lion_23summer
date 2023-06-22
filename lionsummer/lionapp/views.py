245af05d7dd001276e8c26d8c48160fb4ac82c84
haejun1 <a01027833227@gmail.com>
2023-06-22 20:41:22 +0900

    2_1 url, views, templates 연결

--- lionsummer/lionapp/views.py
+++ lionsummer/lionapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):  # 함수를 정의하는 경우 request가 항상 첫번째 인자
+    return render(request, "index.html")  # 함수의 반환은 render, "index.html"이라는 파일을 반환
--- lionsummer/lionsummer/urls.py
+++ lionsummer/lionsummer/urls.py
@@ -1,22 +1,8 @@
-"""
-URL configuration for lionsummer project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from lionapp import views  # lionapp파일 안에 있는 views를 가져온다
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("index/", views.index),  # index가 들어오면, views안에 있는 index함수를 실행
 ]
