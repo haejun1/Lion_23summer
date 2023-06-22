d3d0bcada7665a279bb445d0f9876a1a9b5b1868
haejun1 <a01027833227@gmail.com>
2023-06-22 20:51:03 +0900

    2_2 views에서 python써보기

--- lionsummer/lionsummer/urls.py
+++ lionsummer/lionsummer/urls.py
@@ -5,4 +5,5 @@ from lionapp import views  # lionapp파일 안에 있는 views를 가져온다
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("index/", views.index),  # index가 들어오면, views안에 있는 index함수를 실행
+    path("lotto/", views.lotto),  # 함수에 python써볼 것
 ]
