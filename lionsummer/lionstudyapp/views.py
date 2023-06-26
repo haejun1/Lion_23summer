5636328a57481efb032cf9adc821ec3bc7950b9b
haejun1 <a01027833227@gmail.com>
2023-06-26 12:24:41 +0900

    게시물 작성기능 및 홈으로 이동

--- lionsummer/lionstudyapp/views.py
+++ lionsummer/lionstudyapp/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Lionstudyapp
 
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    return render(request, "lionstudyapp/index.html")
+
+
+def new(request):
+    return render(request, "lionstudyapp/new.html")
+
+
+def create(request):
+    title = request.GET.get("title")
+    content = request.GET.get("content")
+    article = Lionstudyapp(title=title, content=content)
+    article.save()
+    return redirect("/")
