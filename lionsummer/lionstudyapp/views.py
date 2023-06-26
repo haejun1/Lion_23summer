9b112fdf5d0fb4942abe7a810d1bdba873b8d937
haejun1 <a01027833227@gmail.com>
2023-06-26 12:53:17 +0900

    게시물 목록출력, 자세히보기, 수정, 삭제

--- lionsummer/lionstudyapp/views.py
+++ lionsummer/lionstudyapp/views.py
@@ -4,7 +4,9 @@ from .models import Lionstudyapp
 
 # Create your views here.
 def index(request):
-    return render(request, "lionstudyapp/index.html")
+    articles = Lionstudyapp.objects.all()
+    context = {"articles": articles}
+    return render(request, "lionstudyapp/index.html", context)
 
 
 def new(request):
@@ -17,3 +19,32 @@ def create(request):
     article = Lionstudyapp(title=title, content=content)
     article.save()
     return redirect("/")
+
+
+def detail(request, pk):
+    article = Lionstudyapp.objects.get(pk=pk)
+    context = {"article": article}
+    return render(request, "lionstudyapp/detail.html", context)
+
+
+def delete(request, pk):
+    article = Lionstudyapp.objects.get(pk=pk)
+    article.delete()
+    return redirect("/")
+
+
+def edit(request, pk):
+    article = Lionstudyapp.objects.get(pk=pk)
+    context = {"article": article}
+    return render(request, "lionstudyapp/edit.html", context)
+
+
+def update(request, pk):
+    article = Lionstudyapp.objects.get(pk=pk)
+    title = request.GET.get("title")
+    content = request.GET.get("content")
+    article.title = title
+    article.content = content
+    article.save()
+
+    return redirect(f"/{article.pk}/")
